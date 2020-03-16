27b4f319fddbb0c666ce2d585ad73714f29c81a2
Noor Muhammad <connectwithnoor1@gmail.com>
2020-03-16 15:45:42 +0500

    solved: initialize firebase to local server

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,12 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const app = require('express')();
 
-// initial admin sdk in a cloud functions in Node Environment.
-admin.initializeApp(functions.config().firebase);
+const serviceAccountKey = require('./env/SERVICE_ACCOUNT_KEY.json');
+
+// initiallize firebase local server
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey)
+});
 
 const db = admin.firestore();
 
@@ -20,7 +24,27 @@ app.get('/screams', (req, res) => {
     })
     .catch(err => {
       console.log(err);
-      return res.status(500).send({ error: err.code });
+      return res.status(500).send({ error: err.message, code: err.code });
+    });
+});
+
+app.post('/scream', (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString()
+  };
+
+  db.collection('screams')
+    .add(newScream)
+    .then(doc => {
+      return res
+        .status(201)
+        .send({ message: `document ${doc.id} created successfully` });
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(500).send({ error: err.message, code: err.code });
     });
 });
 
