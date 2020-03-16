ada391c452733ba8127bffe8cfd2db2daa895be7
Noor Muhammad <connectwithnoor1@gmail.com>
2020-03-16 16:48:32 +0500

    setup: firebase auth and signup route (validation remaining)

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,56 @@
+// -----------------IMPORTS------------------------- //
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const firebase = require('firebase');
 const app = require('express')();
 
 const serviceAccountKey = require('./env/SERVICE_ACCOUNT_KEY.json');
+const firebaseConfig = require('./env/FIREBASE_CONFIG');
 
-// initiallize firebase local server
+// ----------------INITIALIZATION-------------------------- //
+
+// initiallize firestore local server
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountKey)
 });
 
-const db = admin.firestore();
+// initialize firebase for auth
+firebase.initializeApp(firebaseConfig);
 
-app.get('/screams', async (req, res) => {
+// --------------CONSTANTANTS---------------------------- //
+
+const db = admin.firestore();
+const auth = firebase.auth();
+
+// ----------------ROUTES-------------------------- //
+
+// get all screams
+app.get('/getscreams', async (req, res) => {
   try {
     const data = await db
       .collection('screams')
       .orderBy('createdAt', 'desc')
       .get();
 
-    const screams = await data.docs.map(item => item.data());
+    const screams = await data.docs.map(item => {
+      return {
+        screamId: item.id,
+        body: item.data().body,
+        userHandle: item.data().userHandle,
+        createdAt: item.data().createdAt
+      };
+    });
     return res.status(200).send(screams);
   } catch (err) {
-    console.log(err);
+    console.error(err);
     return res.status(500).send({ error: err.message, code: err.code });
   }
 });
 
-app.post('/scream', async (req, res) => {
+// post new scream
+
+app.post('/newscream', async (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
@@ -39,7 +63,33 @@ app.post('/scream', async (req, res) => {
       .status(201)
       .send({ message: `document ${scream.id} created successfully` });
   } catch (err) {
-    console.log(err);
+    console.error(err);
+    return res.status(500).send({ error: err.message, code: err.code });
+  }
+});
+
+// signup new registration
+
+app.post('/signup', async (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  };
+
+  //   TODO: validate data
+
+  try {
+    const data = await auth.createUserWithEmailAndPassword(
+      newUser.email,
+      newUser.password
+    );
+    return res
+      .status(201)
+      .send({ message: `user ${data.user.uid} signed up successfully` });
+  } catch (err) {
+    console.error(err);
     return res.status(500).send({ error: err.message, code: err.code });
   }
 });
