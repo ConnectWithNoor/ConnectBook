1f1307a131dc06f7676b22469a2ea61032c108fe
Noor Muhammad <connectwithnoor1@gmail.com>
2020-03-16 14:00:40 +0500

    setup: get screams cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const app = require('express')();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// initial admin sdk in a cloud functions in Node Environment.
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
+
+app.get('/screams', (req, res) => {
+  db.collection('screams')
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then(data => {
+      const screams = [];
+      data.forEach(doc => {
+        screams.push(doc.data());
+      });
+      return res.status(200).send(screams);
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(500).send({ error: err.code });
+    });
+});
+
+exports.api = functions.https.onRequest(app);
