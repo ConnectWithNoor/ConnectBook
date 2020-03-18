d19595f79ac340695711ddce03cd194c45dc3b3e
Noor Muhammad <connectwithnoor1@gmail.com>
2020-03-18 15:44:54 +0500

    fix: remove multer.single function

--- functions/routes/uploadImage.js
+++ functions/routes/uploadImage.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const route = express.Router();
 
-route.post('/', upload.single('image'), (req, res) => {
+route.post('/', (req, res) => {
   console.log(req.file);
   return res.send('test');
 });
